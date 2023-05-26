492ce421f7c13bb72da35ccff7f792b8636b9df9
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2023-05-27 00:53:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+
+
 
 function App() {
+  const[Mode, setMode] = useState("light")
+  const[tmode, settmode] = useState("Dark Mode")
+
+  const toogle =()=>{
+    // console.log("Hello world")
+    if(Mode === "light")
+    {
+      setMode("dark")
+      document.body.style.backgroundColor = "black";
+      settmode("Light Mode")
+
+    }
+    else{
+      setMode("light")
+      document.body.style.backgroundColor = "white";
+      settmode("Dark Mode")
+
+
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header">
+
+        <h2 style={{
+          color: Mode === "light"?"black":"white"
+        }}>React Assignment</h2>
+        <p  style={{
+          color: Mode === "light"?"black":"white"
+        }}>Create a react application which toggles the background color of the web page on every toggle </p>
+      </div>
+      <div className="main">
+        <button onClick={toogle} >{tmode}</button>
+      </div>
+
+    
     </div>
   );
 }
